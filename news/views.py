87c171df502731dfd001528d71eb05a397c535ab
Aleksey Porfirov <lexqt@yandex.ru>
2011-11-06 02:19:57 +0400

    Fix news app query

--- news/views.py
+++ news/views.py
@@ -3,7 +3,8 @@ from .models import Article
 
 class FrontpageNewsView(ListView):
     
-    queryset = Article.objects.filter(published=True).order_by('-created')[:5]
+    queryset = Article.objects.filter(published=True).select_related(
+        'author').only('title', 'created', 'content', 'author__first_name', 'author__last_name').order_by('-created')[:5]
     context_object_name = 'news_list'
     template_name = 'frontpage_news.html'
 
