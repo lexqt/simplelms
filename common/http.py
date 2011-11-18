b6728eba73a4dcd981109a3629cfc86ee4eb2de2
Aleksey Porfirov <lexqt@yandex.ru>
2011-11-19 03:35:26 +0400

    Improve HttpErrorsMiddleware

--- common/http.py
+++ common/http.py
@@ -3,60 +3,38 @@
 # Http403* classes from http://chronosbox.org/blog/manipulando-erros-http-403-permissao-negada-no-django?lang=en
 
 from django.conf import settings
-from django.http import HttpResponseForbidden
-from django.template import RequestContext, Template, loader, TemplateDoesNotExist
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
 from django.utils.importlib import import_module
-
-"""
-# This is exception for one Http403 response, 403 code is HttpForbidden, django
-# provide a HttpResponseForbidden class, but isn't handled as Http404
-# exception, this code is write to make it functional.
-"""
 
 class Http403(Exception):
     """Exception for Http Forbidden"""
 
-class Http403Middleware(object):
-    """
-    Replace Http403 raises for a 403.html rendered template
-    """
-    def process_exception(self, request, exception):
-        """
-        Render a 403.html template or a hardcoded html as denied page, but only if
-        exception is instance of Http403 class
-        """
-        if not isinstance(exception, Http403):
-            # Return None makes django reraise exception:
-            return None
+class HttpErrorsMiddleware(object):
+    """Replace HttpXXX raises for a XXX.html rendered template"""
 
+    def process_exception(self, request, exception):
+        if isinstance(exception, Http403):
+            code = 403
+        elif isinstance(exception, Http404):
+            code = 404
+        else:
+            # reraise exception
+            return None
+        
         try:
-            # Handle import error but allow any type error from view
-            callback = getattr(import_module(settings.ROOT_URLCONF), 'handler403')
+            callback = getattr(import_module(settings.ROOT_URLCONF), u'handler{0}'.format(code))
             return callback(request, exception)
         except (ImportError, AttributeError):
-            # doesn't exist a handler403, try get template
-            try:
-                t = loader.get_template('403.html')
-            except TemplateDoesNotExist:
-                # doesn't exist a template in path, use hardcoded template
-                t = Template("""{% load i18n %}
-<!DOCTYPE HTML>
-<HTML>
-    <HEAD>
-        <TITLE>{% blocktrans with request.META.PATH as path %}You get access denied to {{ path }}.{% endblocktrans %}</TITLE>
-    </HEAD>
-    <BODY>
-        <h2>{% trans "Access denied."%}</h2>
-        <p>
-        {% if message %}{{ message }}{% else %}
-            {% blocktrans with request.META.PATH_INFO as path %}We're sorry, but your access are denied to the {{ path }}.{% endblocktrans %}
-        {% endif %}</p>
-    </BODY>
-</HTML>""")
+            pass
+        
+        t = loader.get_template(u'{0}.html'.format(code))
 
-            # now use context and render template            
-            c = RequestContext(request, {
-                'message': exception.message
-            })
+        c = RequestContext(request, {
+            'request': request,
+            'exception': exception
+        })
 
-            return HttpResponseForbidden(t.render(c))
+        response = HttpResponse(t.render(c))
+        response.status_code = code
+        return response
